7f2aae64ba36b4772bcf6d2e6d1ee2f34bd335fe
tigarto <helpu1409@gmail.com>
2023-11-19 00:54:47 -0500

    Agregando mas ejemplos

--- 2/prox_arduino-nano33-BLE/src/main.cpp
+++ 2/prox_arduino-nano33-BLE/src/main.cpp
@@ -1,36 +1,3 @@
-# Arduino Nano en Platformio
-
-![Arduino-Nano](https://docs.arduino.cc/static/bfd8d45fe6dfd1bdf76735464ef75ab0/a6d36/Nano33_ble_sense.png)
-
-* **Nano 33 BLE Sense Cheat Sheet** [[link]](https://docs.arduino.cc/tutorials/nano-33-ble-sense/cheat-sheet)
-
-
-![pinout](https://docs.arduino.cc/static/c18e027f826663ba9f16ffd94b60500f/a6d36/pinout.png)
-
-
-Camara: https://openmv.io/
-
-
-## Ejemplo 1 - Temperatura
-
-Este ejemplo no dio. La tarjeta al parecer no tiene sensor de temperatura
-
-## Ejemplo 2 - Sensor de proximidad
-
-https://docs.arduino.cc/tutorials/nano-33-ble-sense/proximity-sensor
-
-
-```ini 
-[env:nano33ble]
-platform = nordicnrf52
-board = nano33ble
-framework = arduino
-lib_deps = arduino-libraries/Arduino_APDS9960@^1.0.4
-```
-
-Codigo:
-
-```ino
 #include <Arduino.h>
 #include <Arduino_APDS9960.h>
 
@@ -130,30 +97,3 @@ void loop() {
     Serial.println(proximity);
   }
 }
-```
-
-
-
-
-
-## Referencias
-
-* https://github.com/Harvard-CS249R-Fall2020/assignments/tree/master/hello_world
-* https://github.com/Harvard-CS249R-Fall2020/assignments/tree/master/person_detection
-* https://github.com/Harvard-CS249R-Fall2020/assignments/blob/master/wake_words/README.md#deploying-a-speech-model-to-the-microcontroller-using-vscode--platformio
-* https://github.com/Harvard-CS249R-Fall2020/assignments/blob/master/wake_words/README.md
-* https://docs.platformio.org/en/latest/boards/nordicnrf52/nano33ble.html
-* https://sensiml.com/documentation/firmware/arduino-nano33/arduino-nano33.html
-* https://kevinxli.medium.com/manage-two-arduinos-with-ease-using-platformio-4f83ad4a8868
-* https://www.digikey.be/en/maker/tutorials/2022/platformio-advanced-features-for-code-debugging-and-version-control
-* https://edgeimpulse.com/blog/platformio
-* https://github.com/PacktPublishing/TinyML-Cookbook/blob/main/Chapter03/ArduinoSketches
-* https://docs.arduino.cc/hardware/nano-33-ble-sense
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense/cheat-sheet
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense/getting-started-omv
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense/community-projects
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense-rev2/community-projects
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense/ble-device-to-device
-* https://docs.arduino.cc/tutorials/nano-33-ble-sense/humidity-and-temperature-sensor
-* https://github.com/sandeepmistry/Arduino-Nano-33-BLE-Sense-Examples/tree/master
-
