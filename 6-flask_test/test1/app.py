21d1b1705fcacc4f03675b74d31ec0d193292677
tigarto <helpu1409@gmail.com>
2023-12-11 22:06:22 -0500

    Actualizacion de app.py

--- 6-flask_test/test1/app.py
+++ 6-flask_test/test1/app.py
@@ -8,12 +8,18 @@ app = Flask(__name__)
 def home():
     return '<h1>Hello, World!</h1>'
 
-
 @app.route('/reporter/<int:reporter_id>')
 def reporter(reporter_id):
     return f'''
     <h2>Reporter {reporter_id} Bio</h2>
     <a href="/">Return to home page</a>
+    '''
+
+@app.route('/article/<article_name>')
+def article(article_name):
+    return f'''
+    <h2>{article_name}</h2>
+    <a href="/">Return back to home page</a>
     '''
 
 
