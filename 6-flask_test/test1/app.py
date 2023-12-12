8a7516282fd4cd1193463413fee5199a892a7c66
tigarto <helpu1409@gmail.com>
2023-12-11 21:39:42 -0500

    Primera actualizacion

--- 6-flask_test/test1/app.py
+++ 6-flask_test/test1/app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/reporter')
+def reporter():
+    return 'Reporter Bio'
 
 if __name__ == '__main__':
     app.run()
