49b062f37aab466f9d08374b413880fe5aece26b
tigarto <helpu1409@gmail.com>
2023-12-11 21:46:23 -0500

    Actualizacion 2 del script de flask

--- 6-flask_test/test1/app.py
+++ 6-flask_test/test1/app.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    # return 'Hello World!'
+    return '<h1>Hello World!</h1>'
 
 @app.route('/reporter')
 def reporter():
-    return 'Reporter Bio'
+    # return 'Reporter Bio'
+    # return '<h2>Reporter Bio</h2>'
+    return '''
+        <h2>Reporter Bio</h2>
+        <a href="/">Return to home page</a>
+        '''
 
 if __name__ == '__main__':
     app.run()
