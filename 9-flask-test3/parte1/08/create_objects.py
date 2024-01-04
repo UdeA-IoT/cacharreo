c52e57aec6d272300a712d08a9bfee67e0d5cefe
tigarto <helpu1409@gmail.com>
2024-01-03 23:01:56 -0500

    Actualizando actividad

--- 9-flask-test3/parte1/08/create_objects.py
+++ 9-flask-test3/parte1/08/create_objects.py
@@ -8,9 +8,10 @@ r1 = Reader(id = 342, name = 'Ann', surname = 'Adams', email = 'ann.adams@exampl
 print("My first reader:", r1.name)
 
 #Checkpoint 1: 
-
+b2 = Book(id = 533, title = 'The Stranger', author_name = "Albert", author_surname = 'Camus', month = "April", year = 2019)
 #Checkpoint 2: 
-
+r2 = Reader(id = 765, name = 'Sam', surname = 'Adams', email = 'sam.adams@example.com')
 #Checkpoint 3: 
-
+print(b2.author_surname)
 #Checkpoint 4:
+print(len(r2.email))
